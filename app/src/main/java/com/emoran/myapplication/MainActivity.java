475988c7830496b0deaf302d1aa86d242dd1ad7d
enriquemoran-hub <e.moran.lin@gmail.com>
2021-12-01 12:20:05 +0100

    casiAcabad0

--- app/src/main/java/com/emoran/myapplication/MainActivity.java
+++ app/src/main/java/com/emoran/myapplication/MainActivity.java
@@ -1,10 +1,13 @@
 package com.emoran.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.Menu;
@@ -15,6 +18,7 @@ import android.webkit.WebView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,14 +30,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // TextView mycontext = (TextView) findViewById(R.id.textTap);
-       // registerForContextMenu(mycontext);
+        // TextView mycontext = (TextView) findViewById(R.id.textTap);
+        // registerForContextMenu(mycontext);
         // casting a la vista a la que aplicamos un menu contextual
         // y la registramos
         //  WebView miVisorWeb = (WebView) findViewById(R.id.vistaweb);
         // abre un menu contextual con un tap largo
-         miVisorWeb = (WebView) findViewById(R.id.vistaweb);
-         registerForContextMenu(miVisorWeb);
+        miVisorWeb = (WebView) findViewById(R.id.vistaweb);
+        registerForContextMenu(miVisorWeb);
 
 
         // DENTRO del Oncreate
@@ -50,6 +54,58 @@ public class MainActivity extends AppCompatActivity {
         miVisorWeb.loadUrl("https://thispersondoesnotexist.com");
 
 
+    }
+
+    //-------------------------------------------------------------------------------------------------
+    //Dialogo modal
+    public void showAlertDialogButtonClicked(MainActivity mainActivity) {
+
+        // setup the alert builder
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
+        //el dialogo estandar tiene título/icono pero podemos sustituirlo por un XML a medida
+        builder.setTitle("Achtung!");
+        builder.setMessage("Where do you go?");
+        builder.setIcon(R.drawable.icono_descarga);
+        builder.setCancelable(false);
+
+
+//       // un XML a medida para el diálogo
+        // builder.setView(getLayoutInflater().inflate(R.layout.alertdialog_view, null));
+        // add the buttons
+        builder.setPositiveButton("Signup", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // do something like...
+                Intent intent = new Intent(MainActivity.this, SignUp.class);
+                startActivity(intent);
+                dialog.dismiss();
+
+            }
+        });
+
+        builder.setNegativeButton("Do nothing", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                // do something like...
+
+                dialog.dismiss();
+            }
+        });
+
+        builder.setNeutralButton("Other", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                // do something like...
+
+                dialog.dismiss();
+            }
+        });
+
+        // create and show the alert dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     // FUERA del Oncreate
@@ -79,13 +135,57 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    //implementing ActionBar/AppBar menu
+    //añadimos appbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_top, menu);
+        return true;
+    }
+
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_context, menu);
+        if (id == R.id.idLupita) {
+//            showAlertDialogButtonClicked(Main.this);
+
+            Toast toast = Toast.makeText(this, "Infecting", Toast.LENGTH_LONG);
+            toast.show();
+            //llamada al metodo para dialogo modal
+            showAlertDialogButtonClicked(MainActivity.this);
+
+        }
+        if (id == R.id.idDesacarga) {
+            Toast toast = Toast.makeText(this, "Fixing", Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+        // if (id == R.id.item4) {
+        //    showAlertDialogButtonClicked(Main.this);
+        //  }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // implements context menu
+    //@Override
+    //  public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+    //     super.onCreateContextMenu(menu, v, menuInfo);
+    //   MenuInflater inflater = getMenuInflater();
+    //   inflater.inflate(R.menu.menu_context, menu);
+    //  }
+
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+
+        getMenuInflater().inflate(R.menu.menu_context, menu);
+
     }
 
     @Override
@@ -109,7 +209,7 @@ public class MainActivity extends AppCompatActivity {
                         });
                 snackbar.show();
             case R.id.item2:
-                Toast toast2 = Toast.makeText(this, "going CONTEXT SETTINGS",
+                Toast toast2 = Toast.makeText(this, "downloading item",
                         Toast.LENGTH_LONG);
                 toast2.show();
                 return true;
@@ -117,31 +217,8 @@ public class MainActivity extends AppCompatActivity {
                 return super.onContextItemSelected(item);
         }
 
-
     }
 
-
-    //añadimos appbar
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_top, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.idLupita) {
-            return true;
-        }
-        if (id == R.id.idDesacarga) {
-            Toast toast = Toast.makeText(this,"going APPBAR CAMERA",Toast.LENGTH_LONG );
-            toast.show();
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
+
+
